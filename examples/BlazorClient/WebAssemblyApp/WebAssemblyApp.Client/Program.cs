48956a819ed63b5312ca48851960fb0cd976a69c
Comiscience <hu-beau@outlook.com>
2024-04-10 22:15:50 +0800

    refactored asp.net core blazor webassembly example

--- examples/BlazorClient/WebAssemblyApp/WebAssemblyApp.Client/Program.cs
+++ examples/BlazorClient/WebAssemblyApp/WebAssemblyApp.Client/Program.cs
@@ -3,10 +3,9 @@ using FeatBit.ClientSdk;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-
 var options = new FbOptionsBuilder("S37S_0bmkUKTQkCIg5GnKQ5ZjgdjXPU0qDo5LAVn4GzA")
                     .Eval(new Uri("https://featbit-tio-eval.zeabur.app"))
-                    .DataSyncMethod(DataSyncMethodEnum.Polling, 100000)
+                    .DataSyncMethod(DataSyncMethodEnum.Polling, 5000)
                     .Build();
 builder.Services.AddSingleton<IFbClient>(provider => new FbClient(options, autoSync: false));
 
--- examples/BlazorClient/WebAssemblyApp/WebAssemblyApp/Program.cs
+++ examples/BlazorClient/WebAssemblyApp/WebAssemblyApp/Program.cs
@@ -8,11 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
-//var options = new FbOptionsBuilder("S37S_0bmkUKTQkCIg5GnKQ5ZjgdjXPU0qDo5LAVn4GzA")
-//                    .Eval(new Uri("https://featbit-tio-eval.zeabur.app"))
-//                    .DataSyncMethod(DataSyncMethodEnum.Polling, 100000)
-//                    .Build();
-//builder.Services.AddSingleton<IFbClient>(provider => new FbClient(options, autoSync: false));
+var options = new FbOptionsBuilder("").Build();
+builder.Services.AddSingleton<IFbClient>(provider => new FbClient(options, autoSync: false));
 
 var app = builder.Build();
 
