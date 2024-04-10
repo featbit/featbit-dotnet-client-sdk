c6ba029d7e8668f7d1113cb74f4abe8f601e532a
Comiscience <hu-beau@outlook.com>
2024-04-10 22:30:42 +0800

    simplified feature flag usage in webassembly

--- examples/BlazorClient/WebAssemblyApp/WebAssemblyApp.Client/Program.cs
+++ examples/BlazorClient/WebAssemblyApp/WebAssemblyApp.Client/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FeatBit.ClientSdk;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+
 var options = new FbOptionsBuilder("S37S_0bmkUKTQkCIg5GnKQ5ZjgdjXPU0qDo5LAVn4GzA")
                     .Eval(new Uri("https://featbit-tio-eval.zeabur.app"))
+                    .LoggerFactory(NullLoggerFactory.Instance)
                     .DataSyncMethod(DataSyncMethodEnum.Polling, 5000)
                     .Build();
-builder.Services.AddSingleton<IFbClient>(provider => new FbClient(options, autoSync: false));
+builder.Services.AddSingleton<IFbClient>(provider => new FbClient(options, autoSync: true));
 
 await builder.Build().RunAsync();
