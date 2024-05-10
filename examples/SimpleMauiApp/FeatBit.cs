e0fb5c107141252a3840a4077c658d44fe8fed18
deleteLater <mikcczhang@gmail.com>
2024-05-11 00:04:15 +0800

    add maui example

---
+++ examples/SimpleMauiApp/FeatBit.cs
@@ -0,0 +1,40 @@
+﻿using FeatBit.Sdk.Client;
+using FeatBit.Sdk.Client.Model;
+using FeatBit.Sdk.Client.Options;
+
+namespace SimpleMauiApp;
+
+public static class FeatBit
+{
+    private static readonly FbUser InitialUser = FbUser.Builder("anonymous-id").Name("anonymous").Build();
+    private static FbClient? _instance = null;
+
+    public static FbClient Instance
+    {
+        get
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+
+            var options = new FbOptionsBuilder("JbmetT2IvU2CJTxObJLbiQ1XEjhWE6kEaf1IbJu7gTNQ")
+                // set the pollingInterval to 5 seconds for testing purposes
+#if ANDROID
+                // check the documentation at
+                // https://learn.microsoft.com/en-us/dotnet/maui/data-cloud/local-web-services?view=net-maui-8.0
+                .Polling(new Uri("http://10.0.2.2:5100"), TimeSpan.FromSeconds(5))
+#else
+                .Polling(new Uri("http://localhost:5100"), TimeSpan.FromSeconds(5))
+#endif
+                .Build();
+
+            _instance = new FbClient(options, initialUser: InitialUser);
+
+            // Start the client and wait for 3 seconds to initialize.
+            _instance.StartAsync(TimeSpan.FromSeconds(3)).Wait();
+
+            return _instance;
+        }
+    }
+}
---
+++ examples/SimpleMauiApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SimpleMauiApp.ViewModels;
+
+public class MainPageViewModel : INotifyPropertyChanged
+{
+    private int _delta;
+    private int _counter;
+
+    private string _welcomeMessage;
+    private string _deltaText;
+    private string _counterText;
+
+    public string WelcomeMessage
+    {
+        get => _welcomeMessage;
+        private set
+        {
+            _welcomeMessage = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string DeltaText
+    {
+        get => _deltaText;
+        private set
+        {
+            _deltaText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string CounterText
+    {
+        get => _counterText;
+        private set
+        {
+            _counterText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainPageViewModel()
+    {
+        IncrementCommand = new Command(() =>
+        {
+            _counter += _delta;
+            CounterText = GetCounterText(_counter);
+        });
+
+        var fbClient = FeatBit.Instance;
+
+        _delta = fbClient.IntVariation("counter-delta", defaultValue: 1);
+        _deltaText = GetDeltaText(_delta);
+
+        _counter = 0;
+        _counterText = GetCounterText(_counter);
+
+        var version = fbClient.StringVariation("welcome-message-version", defaultValue: "v0");
+        _welcomeMessage = GetWelcomeMessage(version);
+
+        fbClient.FlagTracker.Subscribe("counter-delta", changedEvent =>
+        {
+            if (!int.TryParse(changedEvent.NewValue, out var delta))
+            {
+                return;
+            }
+
+            _delta = delta;
+            DeltaText = GetDeltaText(_delta);
+        });
+
+        fbClient.FlagTracker.Subscribe(
+            "welcome-message-version",
+            changedEvent => WelcomeMessage = GetWelcomeMessage(changedEvent.NewValue)
+        );
+    }
+
+    public ICommand IncrementCommand { private set; get; }
+
+    private static string GetWelcomeMessage(string version) => $"[{version}] Welcome to .NET Multi-platform App UI";
+
+    private static string GetDeltaText(int delta) => $"Delta: {delta}";
+
+    private static string GetCounterText(int count) =>
+        count switch
+        {
+            0 => "Click me!",
+            1 => "Clicked 1 time.",
+            _ => $"Clicked {count} times."
+        };
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
