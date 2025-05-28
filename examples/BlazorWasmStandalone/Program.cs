81331fe4d194e62f844844a7397ac714660eb75c
deleteLater <mikcczhang@gmail.com>
2025-05-28 20:58:52 +0800

    chore

--- examples/BlazorWasmStandalone/Program.cs
+++ examples/BlazorWasmStandalone/Program.cs
@@ -5,18 +5,18 @@ using FeatBit.Sdk.Client.Options;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+const string secret = "";
+if (string.IsNullOrWhiteSpace(secret))
+{
+    Console.WriteLine("Please edit Program.cs to set secret to your FeatBit SDK secret first. Exiting...");
+    return;
+}
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-const string secret = "";
-if (string.IsNullOrWhiteSpace(secret))
-{
-    Console.WriteLine("Please edit Program.cs to set secret to your FeatBit SDK secret first. Exiting...");
-    Environment.Exit(1);
-}
 
 // inject FbClient
 await AddFeatBitAsync();
