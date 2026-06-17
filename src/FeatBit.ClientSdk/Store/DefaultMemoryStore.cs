435cebae78d1de1bdb091649e5e12c21937cff4e
deleteLater <mikcczhang@gmail.com>
2026-06-17 11:58:29 +0800

    fix: return fallback value for archived flag (#12)

--- src/FeatBit.ClientSdk/Store/DefaultMemoryStore.cs
+++ src/FeatBit.ClientSdk/Store/DefaultMemoryStore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using FeatBit.Sdk.Client.Model;
@@ -7,44 +8,56 @@ namespace FeatBit.Sdk.Client.Store
 {
     public class DefaultMemoryStore : IMemoryStore
     {
-        private readonly object _writeLock = new object();
+        private const string FlagArchivedReason = "flag archived";
 
-        private volatile Dictionary<string, FeatureFlag> _items;
+        private readonly ConcurrentDictionary<string, FeatureFlag> _items;
 
         public DefaultMemoryStore(IEnumerable<FeatureFlag> bootstrap)
         {
-            _items = bootstrap.ToDictionary(flag => flag.Id, flag => flag);
+            var kvs = bootstrap.Select(flag => new KeyValuePair<string, FeatureFlag>(flag.Id, flag));
+            _items = new ConcurrentDictionary<string, FeatureFlag>(kvs);
         }
 
         public FeatureFlag Get(string key)
         {
-            return _items.TryGetValue(key, out var flag) ? flag : null;
+            if (_items.TryGetValue(key, out var flag))
+            {
+                // archived flag is treated as not found
+                return flag.MatchReason == FlagArchivedReason ? null : flag;
+            }
+
+            return null;
         }
 
-        public ICollection<FeatureFlag> GetAll() => _items.Values;
+        public ICollection<FeatureFlag> GetAll() => _items.Values
+            .Where(flag => flag.MatchReason != FlagArchivedReason)
+            .ToArray();
 
         public void Upsert(FeatureFlag flag)
         {
-            lock (_writeLock)
-            {
-                FlagValueChangedEvent theEvent = null;
-                if (_items.TryGetValue(flag.Id, out var existingFlag))
+            FlagValueChangedEvent theEvent = null;
+
+            _items.AddOrUpdate(
+                flag.Id,
+                addValueFactory: _ =>
+                {
+                    theEvent = new FlagValueChangedEvent(flag.Id, null, flag.Variation);
+                    return flag;
+                },
+                updateValueFactory: (_, existingFlag) =>
                 {
                     if (existingFlag.Variation != flag.Variation)
                     {
                         theEvent = new FlagValueChangedEvent(flag.Id, existingFlag.Variation, flag.Variation);
                     }
-                }
-                else
-                {
-                    theEvent = new FlagValueChangedEvent(flag.Id, null, flag.Variation);
-                }
 
-                _items[flag.Id] = flag;
-                if (theEvent != null)
-                {
-                    FlagValueChanged?.Invoke(this, theEvent);
+                    return flag;
                 }
+            );
+
+            if (theEvent != null)
+            {
+                FlagValueChanged?.Invoke(this, theEvent);
             }
         }
 
